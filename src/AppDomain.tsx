11799317e8b2565a17184e8b0d43ae2a0596d3de
parker <zppro@parkerdeMacBook-Air.local>
2024-12-11 15:19:40 +0800

    logo

--- src/AppDomain.tsx
+++ src/AppDomain.tsx
@@ -24,6 +24,11 @@ function AppDomain() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a>
+          <svg className="logo react" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 409.6000061035156 409.6000061035156">
+            <path d="M0 0h187.735v187.735H0zM375.465 221.87H256c-18.765 0-34.135 15.365-34.135 34.13v119.47c0 18.77 15.37 34.13 34.135 34.13h119.465c18.77 0 34.135-15.36 34.135-34.13V256c0-18.765-15.365-34.13-34.135-34.13zM315.735 0c-51.835 0-93.87 42.035-93.87 93.885 0 51.814 42.035 93.85 93.87 93.85 51.83 0 93.865-42.035 93.865-93.85C409.6 42.035 367.565 0 315.735 0zM102.4 221.87L0 409.6h204.8z" fill="#00ff00"></path>
+          </svg>
+        </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
